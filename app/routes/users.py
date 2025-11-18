f22e15ec3c4db69d512c64adcd949a0b4e062229
KennyLond <kennyadso15@gmail.com>
2025-11-18 16:00:35 -0500

    feat: implement routes and service layer for authentication

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,31 +1,18 @@
-from pydantic import BaseModel, Field, field_validator
-import re
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.schemas.schemas_user import UserCreate,UserResponse
+from app.services.user_service import register_user_service, login_user_service
 
-class UserBase(BaseModel):
-    user_name : str = Field(...,description='User name')
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
 
-class UserCreate(UserBase):
-    password: str = Field(...,min_length=4, description='secure password')
+@router.post("/register",response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def register_user(data:UserCreate, db:Session = Depends(get_db)):
+    return register_user_service(data, db)
 
-    @field_validator("password")
-    def validate_password(cls,password):
-        if len(password) < 3:
-            raise ValueError("te password must be at least 4 characters long.")
-        if not re.search(r"[A-Z]", password):
-            raise ValueError("the password must include at least one uppercase letter")
-        
-        if len(re.findall(r"\d", password)) < 1:
-            raise ValueError ( "The password must contain at least one number.")
-        
-        return password
-
-class UserLogin(BaseModel):
-    user_name: str
-    password : str
-
-class UserResponse(BaseModel):
-    id: int
-    user_name: str
-
-    class Config:
-        orm_mode = True
+@router.post("/login")
+def login_user(data: UserCreate, db:Session = Depends(get_db)):
+    return login_user_service(data, db )
---
+++ app/services/user_service.py
@@ -0,0 +1,45 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.models.user_models import User
+from app.schemas.schemas_user import UserCreate
+from app.auth.jwt_handler import create_access_token
+import bcrypt
+
+def register_user_service(data: UserCreate, db:Session):
+    existing_user = db.execute(select(User).where(User.user_name == data.user_name)).scalar_one_or_none()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="user name is already registered"
+        )
+    
+    hashed_password = bcrypt.hashpw(data.password.encode("utf-8"),bcrypt.gensalt()).decode("utf-8")
+    new_user = User(username=data.user_name, password = data.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def login_user_service(data, db:Session):
+    user = db.execute(select(User).where(User.user_name == data.user_name)).scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= "User name not registered"
+        )
+    
+    if not bcrypt.checkpw(data.password.encode("utf-8"), user.password.encode("utf-8")):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrct password"
+        )
+    access_token = create_access_token(data={"sub":str(user.id)})
+
+    return {
+        "message":"Login successful",
+        "access_token": access_token,
+        "totken_type":"bearer"
+    }
