06aeff58e90d0210eb9db5ea53ef5693687d3fc8
KennyLond <kennyadso15@gmail.com>
2025-11-18 15:12:23 -0500

    feat: add hashing utilities and JWT token handler

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+from app.db.database import get_db
+from app.models.user_models import User
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
+
+def verify_token(token:str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+
+def get_current_user(token: str = Depends(oauth2_scheme),db:Session=Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code= status.HTTP_401_UNAUTHORIZED,
+        detail="invalid credentials or expired token",
+        headers= {"WWW-Authenticate": "Bearer"},
+    )
+    payload = verify_token(token)
+    if payload is None:
+        raise credentials_exception
+    
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,MetaData
+from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
 import os
 
 load_dotenv()
@@ -8,8 +10,21 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
+meta_data = MetaData()
+Base = declarative_base()
+
+
 try:
     with engine.connect() as connection:
         print("successful connecton to database")
 except Exception as e:
     print("not connected to database")
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
